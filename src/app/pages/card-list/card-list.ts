c74de7f45435134560c69eec149d7979dcb27de7
wsomdev <56587373+wsomdev@users.noreply.github.com>
2026-03-03 19:23:33 +0100

    Add card in mat-list

--- src/app/pages/card-list/card-list.ts
+++ src/app/pages/card-list/card-list.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatIconModule } from '@angular/material/icon';
 import { CUSTOMERS } from '../../models/data/customers.mock';
+import { MatListModule } from '@angular/material/list';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-card-list',
-  imports: [MatExpansionModule, MatIconModule],
+  imports: [MatExpansionModule, MatIconModule, MatListModule, CurrencyPipe],
   templateUrl: './card-list.html',
   styleUrl: './card-list.scss',
 })
